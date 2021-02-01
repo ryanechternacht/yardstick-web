f93163a8b309220b20f72f49209936c7056b817b
ryanechternacht <ryan@echternacht.org>
2021-02-01 09:40:32 -0500

    remove gridlines and improve styling a bit by adding more classes via the 'draw' api

--- src/pages/assessment-details/index.vue
+++ src/pages/assessment-details/index.vue
@@ -6,6 +6,7 @@
       type="Line"
       :data="chartData"
       :options="chartOptions"
+      :event-handlers="eventHandlers"
     />
   </div>
 </template>
@@ -34,28 +35,50 @@ export default {
         axisY: {
           onlyInteger: true
         }
-      }
+      },
+      eventHandlers: [
+        {
+          event: 'draw',
+          fn (event) {
+            if (event.type === 'grid') {
+              console.log(event)
+              event.element._node.classList.add(event.index === 0 ? 'axis' : 'grid-line')
+            } else if (event.type === 'point') {
+              console.log(event)
+              event.element._node.classList.add(event.series.name)
+            } else if (event.type === 'line') {
+              console.log(event)
+              event.element._node.classList.add(event.series.name)
+            }
+          }
+        }
+      ]
     }
   }
 }
 </script>
 
 <style lang="postcss">
-/* TODO i'd like to use the series-name for these, but the selector doesn't seem to work
-   #my-chart g[ct\:series-name="reference"] .ct-point
-*/
-#my-chart .ct-series-a .ct-line,
-#my-chart .ct-series-a .ct-point {
+#my-chart .student.ct-line,
+#my-chart .student.ct-point {
   stroke: blue;
 }
 
-#my-chart .ct-series-b .ct-line {
+#my-chart .reference.ct-line {
   stroke: black;
   stroke-dasharray: 2px 2px;
 }
 
-#my-chart .ct-series-b .ct-point {
+#my-chart .reference.ct-point {
   stroke: black;
+  stroke-width: 0;
+}
+
+#my-chart .axis {
+  stroke-dasharray: initial;
+}
+
+#my-chart .grid-line {
   stroke-width: 0;
 }
 </style>
