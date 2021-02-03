02499b9022f50e028f2b5a5bc171382410031c36
ryanechternacht <ryan@echternacht.org>
2021-02-02 21:22:37 -0500

    Refactor data out into a better object form

--- src/pages/assessment-details/index.vue
+++ src/pages/assessment-details/index.vue
@@ -14,17 +14,44 @@
 <script>
 export default {
   data () {
+    const data = [
+      {
+        student: 226,
+        reference: 222
+      },
+      {
+        student: 228,
+        reference: 220
+      },
+      {
+        student: 229,
+        reference: 222
+      },
+      {
+        student: 230,
+        reference: 224
+      },
+      {
+        student: 227,
+        reference: 222
+      },
+      {
+        student: 232,
+        reference: 224
+      }
+    ]
+
     return {
       chartData: {
         labels: ['Spring \'18', 'Fall \'19', 'Winter \'19', 'Spring \'20', 'Fall \'20', 'Now'],
         series: [
           {
             name: 'student',
-            data: [226, 228, 229, 230, 227, 232]
+            data: data.map(x => x.student)
           },
           {
             name: 'reference',
-            data: [222, 220, 222, 224, 222, 224]
+            data: data.map(x => x.reference)
           }
         ]
       },
@@ -39,13 +66,22 @@ export default {
       eventHandlers: [
         {
           event: 'draw',
-          fn (event) {
+          fn: (event) => {
             if (event.type === 'grid') {
               console.log(event)
               event.element._node.classList.add(event.index === 0 ? 'axis' : 'grid-line')
             } else if (event.type === 'point') {
               console.log(event)
               event.element._node.classList.add(event.series.name)
+
+              // eslint-disable-next-line no-undef
+              const text = new this.$chartist.Svg('text', {
+                x: event.x - 15,
+                y: event.y - 20
+              }, 'class')
+              text._node.textContent = event.value.y
+
+              event.group.append(text)
             } else if (event.type === 'line') {
               console.log(event)
               event.element._node.classList.add(event.series.name)
