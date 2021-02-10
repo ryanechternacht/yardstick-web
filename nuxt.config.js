36101c8f5b881d0a28d9716d8756a1d92e088767
ryanechternacht <ryan@echternacht.org>
2021-02-10 09:57:40 -0500

    Build out a table and css grid version of the assessment overview
    I think i'm going to use the css grid version since it's much better from a responsive design standpoint. To get the css-grid version working, I had to add vue-fragment (which I was considering for the table version as well). I should probably refactor this into something a bit more readable, but this was a big step in proving that it's possible

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,8 +19,9 @@ export default {
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
+    '@/plugins/chartist',
     '@/plugins/fort-awesome',
-    '@/plugins/chartist'
+    '@/plugins/vue-fragment'
   ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
