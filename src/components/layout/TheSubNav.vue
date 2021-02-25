c7370ac7d0a755699e04ed53939ee6333e14b6e8
ryanechternacht <ryan@echternacht.org>
2021-02-25 08:56:49 -0500

    Refactor the subnav on obstacles and opportunities into a component
    we're going to start using this more often

---
+++ src/components/layout/TheSubNav.vue
@@ -0,0 +1,42 @@
+<template>
+  <nav class="sub-nav">
+    <nuxt-link
+      v-for="l in links"
+      :key="l.to"
+      :to="l.to"
+      class="sub-nav-item"
+      :class="{'sub-nav-item--active': l.active}"
+    >
+      {{ l.text }}
+    </nuxt-link>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'TheSubNav',
+  props: {
+    links: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="postcss" scoped>
+.sub-nav {
+  @apply w-full flex justify-center mb-20;
+}
+
+.sub-nav-item {
+  @apply py-10 text-center mx-10 text-gray-graph-dark font-medium;
+  font-size: 20px;
+  line-height: 24px;
+  width: 280px;
+}
+
+.sub-nav-item--active {
+  @apply border-blue border-b text-blue;
+}
+</style>
--- src/pages/obstacles-and-opportunities.vue
+++ src/pages/obstacles-and-opportunities.vue
@@ -1,21 +1,6 @@
 <template>
   <div>
-    <nav class="sub-nav">
-      <nuxt-link
-        to="/obstacles-and-opportunities/opportunities"
-        class="sub-nav-item"
-        :class="{'sub-nav-item--active': isOpportunities}"
-      >
-        Opportunities
-      </nuxt-link>
-      <nuxt-link
-        to="/obstacles-and-opportunities/obstacles"
-        class="sub-nav-item"
-        :class="{'sub-nav-item--active': isObstacles}"
-      >
-        Obstacles
-      </nuxt-link>
-    </nav>
+    <the-sub-nav :links="links" />
 
     <nuxt-child />
   </div>
@@ -24,6 +9,20 @@
 <script>
 export default {
   computed: {
+    links () {
+      return [
+        {
+          active: this.isOpportunities,
+          text: 'Opportunities',
+          to: '/obstacles-and-opportunities/opportunities'
+        },
+        {
+          active: this.isObstacles,
+          text: 'Obstacles',
+          to: '/obstacles-and-opportunities/obstacles'
+        }
+      ]
+    },
     page () {
       return this.$route.fullPath.split('/')[2]
     },
@@ -38,18 +37,5 @@ export default {
 </script>
 
 <style lang="postcss" scoped>
-.sub-nav {
-  @apply w-full flex justify-center;
-}
 
-.sub-nav-item {
-  @apply py-10 text-center mx-10 text-blue font-bold;
-  font-size: 20px;
-  line-height: 24px;
-  width: 280px;
-}
-
-.sub-nav-item--active {
-  @apply border-blue border-b;
-}
 </style>
