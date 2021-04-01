60ee13066c207bdfd8bc5adbc999e8310d366219
ryanechternacht <ryan@echternacht.org>
2021-04-01 19:33:53 -0400

    Refactor student store for pulling data from the backend

--- src/store/student.js
+++ src/store/student.js
@@ -1,26 +1,39 @@
 export const state = () => ({
-  student: {
-    name: {
-      first: 'Darryl',
-      last: 'Hurt',
-      full: 'Darryl Hurt',
-      possessive: 'Darryl\'s'
-    },
-    pronouns: {
-      nominative: 'he',
-      nominativeUpper: 'He',
-      possessive: 'his',
-      possessiveUpper: 'His',
-      accusative: 'him',
-      accusativeUpper: 'Him'
-    },
-    grade: {
-      ordinal: '8th',
-      cardinal: 8
-    }
-  }
+  student: null
 })
 
 export const getters = {
   student: state => state.student
+}
+
+export const mutations = {
+  loadStudent (state, { student }) {
+    state.student = student
+  }
+}
+
+export const actions = {
+  nuxtServerInit ({ commit }) {
+    const student = {
+      name: {
+        first: 'Darryl',
+        last: 'Hurt',
+        full: 'Darryl Hurt',
+        possessive: 'Darryl\'s'
+      },
+      pronouns: {
+        nominative: 'he',
+        nominativeUpper: 'He',
+        possessive: 'his',
+        possessiveUpper: 'His',
+        accusative: 'him',
+        accusativeUpper: 'Him'
+      },
+      grade: {
+        ordinal: '8th',
+        cardinal: 8
+      }
+    }
+    commit('loadStudent', { student })
+  }
 }
