74e81a1114d8ae69acb7bf48362422089d24a693
ryanechternacht <ryan@echternacht.org>
2021-08-15 12:46:52 -0400

    Update static data
    I should update the assessment scores once more, with the newest starting data

--- src/store/index.js
+++ src/store/index.js
@@ -1,38 +1,31 @@
 // TODO Load these on the fly
+import explanation from '@/assets/static-data/assessment-explanation'
+import results from '@/assets/static-data/assessment-results'
+import settings from '@/assets/static-data/settings'
 import studentData from '@/assets/static-data/student'
 import supports from '@/assets/static-data/supports'
 import opportunities from '@/assets/static-data/opportunities'
 import obstacles from '@/assets/static-data/obstacles'
-import explanations from '@/assets/static-data/assessment-explanations'
 import overviews from '@/assets/static-data/assessment-overviews'
-import results from '@/assets/static-data/assessment-results'
 import user from '@/assets/static-data/user'
 
-// TODO consume this here
-// import graphData from '@/assets/static-data/graph-data'
-
 const studentId = 1
-const prefixedSettings = {
-  currentStudent: studentId
-}
 
-const assessmentIds = [1, 2, 3, 4, 5]
+const assessmentIds = [1, 2, 3, 4]
 
 export const actions = {
   async nuxtServerInit ({ commit }, { $axios }) {
     if (this.$config.staticRuntime) {
       commit('user/loadUser', { user })
       commit('students/loadStudents', { students: [studentData] })
-      commit('settings/loadSettings', { settings: prefixedSettings })
+      commit('settings/loadSettings', { settings })
       commit('supports/loadSupports', { supports: { content: supports }, studentId })
       commit('opportunities/loadOpportunities', { opportunities: { content: opportunities }, studentId })
       commit('obstacles/loadObstacles', { obstacles: { content: obstacles }, studentId })
       commit('assessments/loadOverviews', { overviews: { content: overviews }, studentId })
       assessmentIds.forEach((id) => {
         commit('assessments/loadExplanation', {
-          explanation: {
-            content: explanations.find(e => e.assessmentId === id)
-          },
+          explanation: { content: explanation },
           assessmentId: id,
           studentId
         })
